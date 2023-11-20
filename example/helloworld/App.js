e06f1e21fc0e3f6862c43af9068c7fb038deabf3
zwd <17326195691@163.com>
2023-11-20 17:31:53 +0800

    初始化element主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -2,7 +2,29 @@ import { h } from "../../lib/zwd-mini-vue.esm.js";
 
 export const App = {
   render() {
-    return h("div", "hello, " + this.msg);
+    return h(
+      "div",
+      {
+        id: "root",
+        class: ["main", "content"],
+      },
+      [
+        h(
+          "p",
+          {
+            class: "red",
+          },
+          "hello"
+        ),
+        h(
+          "p",
+          {
+            class: "blue",
+          },
+          "world"
+        ),
+      ]
+    );
   },
   setup() {
     return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
@@ -5,7 +6,36 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  processComponent(vnode, container);
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode, container) {
+  const { type, children } = vnode;
+  let el = document.createElement(type);
+
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    children.forEach((v) => {
+      patch(v, el);
+    });
+  }
+
+  const { props } = vnode;
+  for (const key in props) {
+    const value = props[key];
+    el.setAttribute(key, value);
+  }
+
+  container.append(el);
 }
 
 function processComponent(vnode, container) {
