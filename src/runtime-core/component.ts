2aa24b14d690d5e42b39cecc3789c3359feed411
zwd <17326195691@163.com>
2023-11-20 15:41:02 +0800

    初始化component主流程

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,38 @@
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type,
+  };
+  return component;
+}
+
+export function setupComponent(instance) {
+  // initProps
+  // initSlots
+
+  setupStatefulComponent(instance);
+}
+
+function setupStatefulComponent(instance) {
+  const Component = instance.type;
+
+  const { setup } = Component;
+  if (setup) {
+    const setupResult = setup();
+    handleSetupResult(instance, setupResult);
+  }
+}
+
+function handleSetupResult(instance, setupResult) {
+  if (typeof setupResult === "object") {
+    instance.setupResult = setupResult;
+  }
+  finishComponentSetup(instance);
+}
+
+function finishComponentSetup(instance) {
+  const Component = instance.type;
+  if (Component.render) {
+    instance.render = Component.render;
+  }
+}
