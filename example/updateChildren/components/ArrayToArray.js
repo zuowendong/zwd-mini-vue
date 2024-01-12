77461da239daf1c39ee0066947aaf0d487e2ea74
zwd <17326195691@163.com>
2024-01-12 15:52:24 +0800

    中间乱序对比删除操作

--- example/updateChildren/components/ArrayToArray.js
+++ example/updateChildren/components/ArrayToArray.js
@@ -78,12 +78,61 @@ import { h, ref } from "../../../lib/zwd-mini-vue.esm.js";
  * A(BC)
  * (BC)
  */
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C" }, "C"),
+// ];
+// const nextChildren = [h("p", { key: "B" }, "B"), h("p", { key: "C" }, "C")];
+
+/**
+ * 乱序-删除
+ * (AB)DCE(FG)
+ * (AB)CHD(FG)
+ */
+// const prevChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "C", id: "prev-id" }, "C"),
+//   h("p", { key: "E" }, "E"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+// const nextChildren = [
+//   h("p", { key: "A" }, "A"),
+//   h("p", { key: "B" }, "B"),
+//   h("p", { key: "C", id: "next-id" }, "C"),
+//   h("p", { key: "H" }, "H"),
+//   h("p", { key: "D" }, "D"),
+//   h("p", { key: "F" }, "F"),
+//   h("p", { key: "G" }, "G"),
+// ];
+
+/**
+ * 乱序-删除
+ * (AB)CED(FG)
+ * (AB)EC(FG)
+ */
 const prevChildren = [
   h("p", { key: "A" }, "A"),
   h("p", { key: "B" }, "B"),
-  h("p", { key: "C" }, "C"),
+  h("p", { key: "C", id: "prev-id" }, "C"),
+  h("p", { key: "E" }, "E"),
+  h("p", { key: "D" }, "D"),
+  h("p", { key: "X" }, "X"),
+  h("p", { key: "O" }, "O"),
+  h("p", { key: "F" }, "F"),
+  h("p", { key: "G" }, "G"),
 ];
-const nextChildren = [h("p", { key: "B" }, "B"), h("p", { key: "C" }, "C")];
+const nextChildren = [
+  h("p", { key: "A" }, "A"),
+  h("p", { key: "B" }, "B"),
+  h("p", { key: "E" }, "E"),
+  h("p", { key: "C", id: "next-id" }, "C"),
+  h("p", { key: "F" }, "F"),
+  h("p", { key: "G" }, "G"),
+];
 
 export const ArrayToArray = {
   setup() {
